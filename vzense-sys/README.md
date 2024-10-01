489e9396eb7f97eb73699c099963789d2954232a
h-a-n-n-e-s <johannes.schoenke@gmail.com>
2024-10-01 15:50:18 +0900

    vzense sys adds

---
+++ vzense-sys/README.md
@@ -0,0 +1,3 @@
+# vzense-sys
+
+Generates and uses Vzense C library bindings as a Rust crate. This crate is used as a base layer in `vzense-rust`.
--- vzense-sys/src/lib.rs
+++ vzense-sys/src/lib.rs
@@ -1,3 +1,9 @@
+/*!
+# vzense-sys
+
+Generates and uses Vzense C library bindings as a Rust crate. This crate is used as a base layer in `vzense-rust`.
+*/
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
